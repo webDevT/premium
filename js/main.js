a438885ae34579962b5918bcacde332c9d13f1c1
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-07-12 10:49:40 +0300

    premium modal

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+$(".premium-modal__close").click(function() {
+$(".premium-modal").hide();
+
+});
+
+
+
+});
+
+$(function(){
+	$(".tab").click(function() {
+	$(".tab").removeClass("active").eq($(this).index()).addClass("active");
+ 	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+});
